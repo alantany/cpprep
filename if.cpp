1fbbec8a19267ca4d00247019f1984b86cb58679
alantany <alantany@gmail.com>
2017-03-17 23:31:13 +0800

    加入了变量显示

--- if.cpp
+++ if.cpp
@@ -6,22 +6,22 @@ int main()
 	while (cin >> ch)
 	{
 		if (ch == 'a')
-			cout << "这是一个元音字母!" << endl;
+			cout <<ch<< " 是一个元音字母!" << endl;
 		else if (ch == 'e')
-			cout << "这是一个元音字母!" << endl;
+			cout <<ch<< " 是一个元音字母!" << endl;
 		else if (ch == 'i')
-			cout << "这是一个元音字母!" << endl;
+			cout <<ch<< " 是一个元音字母!" << endl;
 		else if (ch == 'o')
-			cout << "这是一个元音字母!" << endl;
+			cout <<ch<< " 是一个元音字母!" << endl;
 		else if (ch == 'u')
-			cout << "这是一个元音字母!" << endl;
+			cout <<ch<< " 是一个元音字母!" << endl;
 		else if (ch == 'q')
 		{
 			cout << "bye!" << endl;
 			break;
 		}
 		else
-			cout << "这不是元音字母!" << endl;
+			cout << ch<<" 不是元音字母!" << endl;
 
 	}
 	system("PAUSE");

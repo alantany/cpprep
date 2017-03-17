7af779e283f93fde695e314536225dee8e8802e1
alantany <alantany@gmail.com>
2017-03-17 23:06:04 +0800

    switch func

---
+++ switch.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	while (cin >> ch)
+	{
+	switch (ch)
+	{
+		case 'a':
+			cout << "input A!" << endl;
+			break;
+		case 'b':
+			cout << "input B!" << endl;
+			break;
+		case 'c':
+			cout << "input C!" << endl;
+			break;
+	}
+	}
+
+	system("PAUSE");
+
+}

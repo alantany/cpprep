dbee04367cb69e3d39272838e59a443879f6b834
alantany <alantany@gmail.com>
2017-03-18 18:58:48 +0800

    加入异常处理

--- exception.cpp
+++ exception.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 int main()
 {
-	int i = 0, j = 1;
-	if (i != j)
-		throw runtime_error("两个数不相同");
-	cout << i + j << endl;
+	double  i ,j;
+	cout << "请输入2个数:";
+	while(cin>>i>>j)
+	try
+	{
+		if (j == 0)
+			throw runtime_error("除数不能为0，请重新输入");
+		cout<<(i / j)<<endl;
+	}
+	catch (runtime_error err)
+	{
+		cout << err.what()<<endl;
+
+	}
+
+	//cout << i + j << endl;
 	system("pause");
 }
